ed8915af5a6583358f6af188ecee402be4043f53
Raúl Ruiz <raul.j.ruiz@gmail.com>
2020-05-22 10:41:32 +0200

    Add new feature: compareFolder

--- clasiffoto.py
+++ clasiffoto.py
@@ -13,6 +13,7 @@ import sys
 import os
 import argparse
 import shutil
+from tabulate import tabulate
 
 def getPicInfo(ruta):
     info = {'model':'','year':'','resx':'','resy':'','sizex':'','sizey':''}
@@ -124,9 +125,14 @@ def processFolder(path):
 
 
 
+def printHeader(text):
+    print(Fore.BLUE + text)
+    
+
 def printPicStatus(image,imageInfo,kind,text):
         
     print(Fore.WHITE + image , Fore.GREEN  + imageInfo["model"] , Fore.YELLOW + imageInfo["year"], Fore.LIGHTYELLOW_EX + 'Res X:' + imageInfo["resx"] + ' Res Y:' + imageInfo["resy"] , Fore.YELLOW + 'W:' + imageInfo["sizex"],Fore.YELLOW + 'H:'+imageInfo["sizey"] ,sep=' ',end=' ')
+        
     if (kind == 'S'):
         print(Fore.WHITE + ' => ',Fore.GREEN + text)
         return
@@ -136,29 +142,91 @@ def printPicStatus(image,imageInfo,kind,text):
     if (kind == 'E'):
         print(Fore.WHITE + ' => ',Fore.RED + text)
         return
+    if (kind == 'I'):
+        print('')
+        return
     print('')
+
+
+def compareFolder(source,destination):
+    sourceImages = getFolderImages(source)
+    destinationImages = getFolderImages(destination)
+
+    # Check images that exist only on source or destination folder
+    onlySource = list(set(sourceImages)-set(destinationImages))
+    onlyDestination = list(set(destinationImages)-set(sourceImages))
+    common = list(set(destinationImages) & set(sourceImages))
+
+     
+    #Print results
+
+    if common:
+        printHeader('Image that exist on both source and destination folder')
+        report = []
+        for image in common:
+            imgInfoDest = getPicInfo(destination+"/"+image)
+            imgInfoSource = getPicInfo(source+"/"+image)
+            report.append( [image] + list(imgInfoSource.values()) + list(imgInfoDest.values()))
+
+        """
+            if (not imgInfoDest["year"] or not imgInfoDest["model"]):
+                printPicStatus(image,imgInfoDest,'E','No EXIF info obtained')
+            else:
+                printPicStatus(image,imgInfoDest,'I','')
+            imgInfoDest = getPicInfo(destination+"/"+image)
+            if (not imgInfoDest["year"] or not imgInfoDest["model"]):
+                printPicStatus(image,imgInfoDest,'E','No EXIF info obtained')
+            else:
+                printPicStatus(image,imgInfoDest,'I','')"""
+        print(tabulate(report,headers=['Image','Src.Model','Year','ResX','ResY','Width','Height','Dst.Model','Year','ResX','ResY','Width','Height']))
+
+    if onlySource and not args['onlycommon']:
+        printHeader('Image only exist on source folder:'+source)
+        for image in onlySource:
+            imgInfo = getPicInfo(source+"/"+image)
+            if (not imgInfo["year"] or not imgInfo["model"]):
+                printPicStatus(image,imgInfo,'E','No EXIF info obtained')
+            else:
+                printPicStatus(image,imgInfo,'I','')
+
+    if onlyDestination and not args['onlycommon']:
+        printHeader('Image only exist on destination folder: '+destination)
+        for image in onlyDestination:
+            imgInfo = getPicInfo(destination+"/"+image)
+            if (not imgInfo["year"] or not imgInfo["model"]):
+                printPicStatus(image,imgInfo,'E','No EXIF info obtained')
+            else:
+                printPicStatus(image,imgInfo,'I','')
+
 
 if __name__ == "__main__":
     #Control arguments with parser
     argparser = argparse.ArgumentParser(description='Process pics folder based on Exif data')
 
     # Add the arguments
-    argparser.add_argument('Path',
+    argparser.add_argument('path',
                        metavar='path',
                        type=str,
                        help='Path to the folder that contains images')
     argparser.add_argument('--year', type=str, required=False, action='store', help='Only consider pics with especific year in exif')                       
+    argparser.add_argument('--compare', type=str, required=False, action='store', help='Compare with pics in a different path')                       
     argparser.add_argument('--generate', required=False, action='store_true', help='Generate folders for years and camera model')                       
     argparser.add_argument('--onlyerrors', required=False, action='store_true', help='Show only errors')                       
+    argparser.add_argument('--onlycommon', required=False, action='store_true', help='Show only common images')                       
     
     # Execute the parse_args() method
     args = vars(argparser.parse_args())
    
    
     # 
-    if not os.path.isdir(args['Path']):
+    if not os.path.isdir(args['path']):
         print('The path specified does not exist')
         sys.exit()
-    processFolder(args['Path'])
+    # In case there is no comparison action, processfolder is launched    
+    if not args['compare']:
+        processFolder(args['path'])
+    else:
+        compareFolder(args['path'],args['compare'])
+
 
  
